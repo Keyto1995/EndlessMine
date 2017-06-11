0116c4d49bee6862ab1ca8084dc24f15190a8fc5
Keyto1995 <Keyto1995@Outlook.com>
2017-06-11 18:29:35 +0800

    添加 用户登录情况按钮

--- EndlessMine-WebServer/src/main/java/keyto/endlessmine/webserver/domain/WorldChatResponse.java
+++ EndlessMine-WebServer/src/main/java/keyto/endlessmine/webserver/domain/WorldChatResponse.java
@@ -27,10 +27,6 @@ public class WorldChatResponse {
     private String name;
     private String responseMessage;
 
-    public WorldChatResponse(String responseMessage) {
-        this.responseMessage = responseMessage;
-    }
-
     public WorldChatResponse(String name, String responseMessage) {
         this.name = name;
         this.responseMessage = responseMessage;
